9769c983093a03a6900196d233317732a0374140
Epyi <epyi.contact@gmail.com>
2024-06-03 16:18:06 +0200

    feat: Add missing locale

--- locales/fr.lua
+++ locales/fr.lua
@@ -1,5 +1,9 @@
 locales["fr"] = {
-    ["command_setfaction_help"] = "Définir la faction d'un joueur",
+	["command_setfaction_help"] = "Définir la faction d'un joueur",
+	["command_setfaction_args_player"] = "Joueur cible",
+	["command_setfaction_args_faction"] = "Faction à définir",
+	["command_setfaction_args_rank"] = "Grade dans la faction",
+
 	["notif_setfaction_error"] = "Impossible de changer la faction de %s",
 	["notif_setfaction_success"] = "La faction de %s a bien été changée (faction: %s, grade: %s)",
 }
--- server/commands.lua
+++ server/commands.lua
@@ -2,7 +2,7 @@ ESX.RegisterCommand(
 	{ "setfaction", "setjob2", "setgang", "setorga" },
 	Config.admins,
 	function(xPlayer, args, _)
-		setFaction(args.player.source, args.job, args.rank, function(success)
+		setFaction(args.player.source, args.faction, args.rank, function(success)
 			xPlayer.showNotification(success and _U("notif_setfaction_success", GetPlayerName(args.player.source), args.job, args.rank) or _U("notif_setfaction_error", GetPlayerName(args.player.source)))
 		end)
 	end,
@@ -10,9 +10,9 @@ ESX.RegisterCommand(
 	{
 		help = _U("command_setfaction_help"),
 		arguments = {
-			{ name = "player", help = "Joueur cible", type = "player" },
-			{ name = "job", help = "Nouveau faction", type = "string" },
-			{ name = "rank", help = "Nouveau grade de faction", type = "number" },
+			{ name = "player", help = _U("command_setfaction_args_player"), type = "player" },
+			{ name = "faction", help = _U("command_setfaction_args_faction"), type = "string" },
+			{ name = "rank", help = _U("command_setfaction_args_rank"), type = "number" },
 		},
 	}
 )
